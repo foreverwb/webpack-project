b67779d7d20cbdad035ed7a4bdcd661ae2fbd307
wangbin <wangbin@caicloud.io>
2019-08-08 19:59:11 +0800

    动态import

--- src/search/index.js
+++ src/search/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM from 'react-dom';
 import './index.less'
 import mio from './123.gif'
@@ -9,14 +9,25 @@ if(false) {
   a()
 }
 
-const Search = () => (
+const Search = () => {
+  const [state, setState] = useState({Test: null})
+  loadComponent = () => {
+    import('./test.js').then((res) => {
+      setState({Test: res.default})
+    })
+  }
+  return (
+    <div className="a">
+      {
+        state.Test ? <Test /> : null
+      }
+      <p onClick={this.loadComponent}>Mio Wang</p>
+      <img src={mio} alt="jingyan"/>
+    </div>
+  )
 
-  <div className="a">
-    {}
-    <p>Mio Wang</p>
-    <img src={mio} alt="jingyan"/>
-  </div>
-)
+}
+
 
 ReactDOM.render(
   <Search />,
